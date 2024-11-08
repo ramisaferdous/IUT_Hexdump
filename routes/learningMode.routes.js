59b7984660a142588fa840db3647823dd481340d
Nafisa Kibria <nafisa21@iut-dhaka.edu>
2024-11-08 17:26:17 +0600

    added bfs exercises

--- routes/learningMode.routes.js
+++ routes/learningMode.routes.js
@@ -1,7 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 const learningModeController = require('../controller/learningMode.controller.js');
 
+
 router.get('/learning-mode', learningModeController.getLearningModePage);
+
+
+
+router.get('/learning-mode/:graphType', (req, res, next) => {
+    const { graphType } = req.params;
+    
+    
+    if (graphType === 'bfs-exercise') {
+        res.sendFile(path.join(__dirname, '../exercise_html_files/bfs-exercise.html'));
+    }
+    else if(graphType === 'dfs-exercise1')
+    {
+        res.sendFile(path.join(__dirname, '../exercise_html_files/dfs-exercise1.html'));
+    }
+    
+    else {
+        
+        learningModeController.renderGraphContent(req, res);
+    }
+});
 
 module.exports = router;
