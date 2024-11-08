649dfa35ef25d1ad79aefc2d6313937407779910
Nafisa Nawal <nafisanawal@iut-dhaka.edu>
2024-11-08 10:18:43 +0600

    added prisma

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(session({
 
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, 'html_files', 'landing.html'));
+    res.sendFile(path.join(__dirname, 'html_files', 'landingpage.html'));
 });
 
 
