f22e523efcd2cc3a34c573620f633d286fd45892
Nafisa Nawal <nafisanawal@iut-dhaka.edu>
2024-11-08 09:18:17 +0600

    login route

--- routes/login.routes.js
+++ routes/login.routes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const path = require('path');
 const router = express.Router();
-const signupController = require('../controller/login.controller.js'); 
+const loginController = require('../controller/login.controller.js'); 
 
 router.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, '../html_files/login.html')); 
 });
 
-router.post('/login', signupController.handleSignup);
+router.post('/login', loginController.handleLogin);
 
 module.exports = router;
